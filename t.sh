ae0c28a3b40340959b881b9171f833c3a2727614
Sergey Zemtsov <wbssergey@users.noreply.github.com>
2015-11-09 10:33:28 -0500

    Delete t.sh

--- t.sh
+++
@@ -1,7 +0,0 @@
-echo "# capistrano_radio" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:wbssergey/capistrano_radio.git
-git push -u origin master
-
